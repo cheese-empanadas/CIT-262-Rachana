d79c295a8ee21a5f6ce83c00d1dfe2e56645607c
Rachana <zimboganyo@gmail.com>
2023-01-19 10:47:05 -0700

    fetch

---
+++ tests/loginapibad.test.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+
+it ("Should get a login token", async () => {//async allows us to read from the database by moving on a nd coming back
+
+    const loginResponse = await fetch('https://dev.stedi.me/login',{ // await fetch allows it to move on while getting the data
+        
+        method:'POST', //method: POST says to give information to the server 
+        headers:{
+            'content-Type' : 'application/text'
+        },
+        
+        body:JSON.stringify({ //Makes the following into JSON text to be read by the server
+        
+            "userName":"willy1995@gmail.com",
+            "password": "badpass"
+        })
+
+
+    })
+expect(loginResponse.status).toBe(401); //401 means authorized 
+})
